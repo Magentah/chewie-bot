6274e165bdabfafaa997fa314a89b484c48d0f19
Magentah <i.snowblind7@gmail.com>
2020-09-16 17:48:45 +0900

    Update <a> to <button> due to warning about no href

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-    public render() {
-        async function twitchRedirect() {
-            const response = await fetch('/api/oauth/twitch')
-                                .then((res) => res.json());
+  public render() {
+    async function twitchRedirect() {
+      const response = await fetch("/api/oauth/twitch").then((res) =>
+        res.json()
+      );
 
-            window.location.replace(response.url);
-        }
-
-        return (
-            <div className='App'>
-                <header className='App-header'>
-                    <img src={logo} className='App-logo' alt='logo' />
-                    <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    >
-                    Learn React
-                    </a>
-                    <a class-name='App-link' onClick={twitchRedirect}>Connect to Twitch</a>
-                </header>
-            </div>
-        );
+      window.location.replace(response.url);
     }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button class-name="App-link" onClick={twitchRedirect}>
+            Connect to Twitch
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
