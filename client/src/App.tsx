03904641d3682c1d3ab2f9269c7866d56731b82c
Magentah <i.snowblind7@gmail.com>
2020-09-17 19:13:38 +0900

    Test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>Testing automatic deployment.</p>
+                    <p>Hi mi-tan!</p>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                         Learn React
                     </a>
