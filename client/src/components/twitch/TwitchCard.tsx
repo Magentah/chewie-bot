c3189efb6f60dab16e12cad66a8e86b3eafaa518
Natcha Simsiri <nsimsiri@umass.edu>
2020-10-11 15:58:18 -0400

    moved bot login components to components/twitch/T witchCard.tsx

--- client/src/components/twitch/TwitchCard.tsx
+++ client/src/components/twitch/TwitchCard.tsx
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Image, Button } from "react-bootstrap";
 import { Grid, Card, CardContent, Divider } from "@material-ui/core";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 const TwitchCard: React.FC<any> = (props: any) => {
     const isDev = true;
     const cssTwitchButton = {
         backgroundColor: "#9147ff",
+    };
+
+    const defaultUser: any = {
+        streamlabsToken: null,
+        username: null,
+    };
+
+    const [user, setUser] = useState(defaultUser);
+
+    useEffect(() => {
+        axios
+            .get("/api/isloggedin", {
+                withCredentials: true,
+            })
+            .then((response: AxiosResponse<any>) => {
+                if (response.status === 200) {
+                    const userWrapper: any = { user: response.data };
+                    console.log("login", userWrapper);
+                    setUser(userWrapper);
+                } else if (response.status === 403) {
+                    //
+                }
+            })
+            .catch((err: AxiosError<any>) => {
+                console.log("ERR", err);
+            });
+    }, []);
+
+    const message = (): JSX.Element => {
+        if (user) {
+            return <p>No user logged in.</p>;
+        } else {
+            return (
+                <p>
+                    Logged in to Twitch.tv{" "}
+                    {user.streamlabsToken ? "and Streamlabs" : ""} as{" "}
+                    {user.username}.
+                </p>
+            );
+        }
+    };
+
+    const streamlabsLogin = (): any => {
+        const params: AxiosRequestConfig = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "http://localhost",
+                "Access-Control-Allow-Methods":
+                    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+                "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+                "Access-Control-Allow-Credentials": true,
+            },
+            withCredentials: true,
+            data: undefined,
+        };
+        axios.get("/api/auth/streamlabs", params);
+    };
+
+    const render = () => {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {message()}
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <a class-name="App-link" href="/api/auth/twitch">
+                        Connect to Twitch
+                    </a>
+                    <a class-name="App-link" href="/api/auth/streamlabs">
+                        Connect to Streamlabs
+                    </a>
+                    <a class-name="App-link" href="/api/logout">
+                        Logout
+                    </a>
+                </header>
+            </div>
+        );
     };
 
     const twitchRedirect = async () => {
