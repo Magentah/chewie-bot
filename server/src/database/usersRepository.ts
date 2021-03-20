f098b352de155eeb8d7041e96644fcd51fd0f318
Magentah <magenta.fall@protonmail.com>
2021-03-20 18:47:58 +0900

    Database: Updated some of the queries for Users.
    Removed the query to check if users exists, instead using combined queries
    
    that will handle conflicts and check if a user exists. This should reduce
    
    the number of queries made to the database.

--- server/src/database/usersRepository.ts
+++ server/src/database/usersRepository.ts
@@ -15,16 +15,6 @@ export class UsersRepository {
      */
     public async get(username: string): Promise<IUser | undefined> {
         const databaseService = await this.databaseProvider();
-        Logger.debug(
-            LogType.Database,
-            databaseService
-                .getQueryBuilder(DatabaseTables.Users)
-                .join(DatabaseTables.UserLevels, "userLevels.id", "users.userLevelKey")
-                .join(DatabaseTables.VIPLevels, "vipLevels.id", "users.vipLevelKey")
-                .where("users.username", "like", username)
-                .first(["vipLevels.name as vipLevel", "userLevels.name as userLevel", "users.*"])
-                .toSQL().sql
-        );
 
         const userResult = await databaseService
             .getQueryBuilder(DatabaseTables.Users)
@@ -46,6 +36,65 @@ export class UsersRepository {
         }
 
         // Need to map from SQLResult to the correct model.
+        return this.mapDBUserToUser(userResult);
+    }
+
+    /**
+     * Updates user data in the database if the user already exists.
+     * @param user Updated user
+     * @param points Number of points to add or remove (if negative)
+     */
+    public async incrementPoints(user: IUser, points: number): Promise<void> {
+        const databaseService = await this.databaseProvider();
+
+        await databaseService
+            .getQueryBuilder(DatabaseTables.Users)
+            .increment("points", points)
+            .whereExists((q) => {
+                return q.select().from(DatabaseTables.Users).where({ id: user.id });
+            });
+    }
+
+    /**
+     * Increments or decrements the number of points for a user.
+     * @param user Updated user
+     */
+    public async update(user: IUser): Promise<void> {
+        const databaseService = await this.databaseProvider();
+
+        // Update should not manipulate original object.
+        const userData = { ...user };
+        delete userData.userLevel;
+        delete userData.vipLevel;
+        delete userData.twitchUserProfile;
+
+        await databaseService
+            .getQueryBuilder(DatabaseTables.Users)
+            .update(userData)
+            .whereExists((q) => {
+                if (user.id) {
+                    return q.select().from(DatabaseTables.Users).where({ id: user.id });
+                } else {
+                    return q.select().from(DatabaseTables.Users).where({ username: user.username });
+                }
+            });
+    }
+
+    /**
+     * Add a new user to the database if the user doesn't already exist.
+     * @param user The user to add to the database
+     */
+    public async add(user: IUser): Promise<void> {
+        const databaseService = await this.databaseProvider();
+        await databaseService.getQueryBuilder(DatabaseTables.Users).insert(user).onConflict("username").ignore();
+    }
+
+    public async addMultiple(users: IUser[]): Promise<void> {
+        const databaseService = await this.databaseProvider();
+        await databaseService.getQueryBuilder(DatabaseTables.Users).insert(users).onConflict("username").ignore();
+    }
+
+    private mapDBUserToUser(userResult: any): IUser {
         const user: IUser = {
             hasLogin: userResult.hasLogin,
             points: userResult.points,
@@ -73,91 +122,6 @@ export class UsersRepository {
         };
 
         return user;
-    }
-
-    /**
-     * Updates user data in the database if the user already exists.
-     * @param user Updated user
-     * @param points Number of points to add or remove (if negative)
-     */
-    public async incrementPoints(user: IUser, points: number): Promise<void> {
-        if (!(await this.userExists(user))) {
-            return;
-        }
-
-        const databaseService = await this.databaseProvider();
-
-        Logger.debug(
-            LogType.Database,
-            databaseService
-                .getQueryBuilder(DatabaseTables.Users)
-                .increment("points", points)
-                .where({ id: user.id })
-                .toSQL().sql
-        );
-
-        await databaseService.getQueryBuilder(DatabaseTables.Users).increment("points", points).where({ id: user.id });
-    }
-
-    /**
-     * Increments or decrements the number of points for a user.
-     * @param user Updated user
-     */
-    public async update(user: IUser): Promise<void> {
-        const databaseService = await this.databaseProvider();
-        Logger.debug(
-            LogType.Database,
-            databaseService.getQueryBuilder(DatabaseTables.Users).update(user).where({ id: user.id }).toSQL().sql
-        );
-        if (!(await this.userExists(user))) {
-            return;
-        }
-
-        // Update should not manipulate original object.
-        const userData = { ...user };
-        delete userData.userLevel;
-        delete userData.vipLevel;
-        delete userData.twitchUserProfile;
-
-        await databaseService.getQueryBuilder(DatabaseTables.Users).update(userData).where({ id: user.id });
-    }
-
-    /**
-     * Add a new user to the database if the user doesn't already exist.
-     * @param user The user to add to the database
-     */
-    public async add(user: IUser): Promise<void> {
-        const databaseService = await this.databaseProvider();
-        if (await this.userExists(user)) {
-            return;
-        }
-
-        Logger.debug(LogType.Database, databaseService.getQueryBuilder(DatabaseTables.Users).insert(user).toSQL().sql);
-        await databaseService.getQueryBuilder(DatabaseTables.Users).insert(user);
-    }
-
-    /**
-     * Private function to check if a user exists in the database
-     * @param user User to get
-     * @returns True if the user exists in the database, false if the user does not exist.
-     */
-    private async userExists(user: IUser): Promise<boolean> {
-        const databaseService = await this.databaseProvider();
-        if (user.id && user.id > 0) {
-            const result = await databaseService.getQueryBuilder(DatabaseTables.Users).first().where({ id: user.id });
-            if (result) {
-                return true;
-            }
-        } else {
-            const result = await databaseService
-                .getQueryBuilder(DatabaseTables.Users)
-                .first()
-                .where("username", "like", user.username);
-            if (result) {
-                return true;
-            }
-        }
-        return false;
     }
 }
 
