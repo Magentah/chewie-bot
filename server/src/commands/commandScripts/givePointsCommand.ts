ba9717093a924913e1016754da71ed355d436f67
Magentah <magenta.fall@protonmail.com>
2021-03-20 18:50:45 +0900

    Commands: Add commands for checking and giving user points.
    Added !points and !givepoints / !give commands, used to check the users
    
    points and to give points to another user.

---
+++ server/src/commands/commandScripts/givePointsCommand.ts
@@ -0,0 +1,54 @@
+import { Command } from "../command";
+import { TwitchService, UserService } from "../../services";
+import { ICommandAlias, IUser } from "../../models";
+import { BotContainer } from "../../inversify.config";
+import { Lang } from "../../lang";
+
+export default class GivePointsCommand extends Command {
+    private twitchService: TwitchService;
+    private userService: UserService;
+
+    constructor() {
+        super();
+        this.twitchService = BotContainer.get(TwitchService);
+        this.userService = BotContainer.get(UserService);
+    }
+
+    public async execute(channel: string, user: IUser, targetUsername: string, points: number) {
+        if (!targetUsername || !points) {
+            this.twitchService.sendMessage(channel, Lang.get("points.give.wrongarguments", user.username));
+            return;
+        }
+
+        if (targetUsername === user.username) {
+            this.twitchService.sendMessage(channel, Lang.get("points.give.noself", user.username));
+            return;
+        }
+
+        if (points > user.points) {
+            this.twitchService.sendMessage(channel, Lang.get("points.give.insufficientpoints", user.username));
+            return;
+        }
+
+        // TODO: This is going to cause issues currently for users that exist in chat but haven't logged in. This should really check to see if
+        // the user is in chat as well, and if they are, create a new user in the db for them.
+        const targetUser = await this.userService.getUser(targetUsername);
+        if (!targetUser) {
+            this.twitchService.sendMessage(channel, Lang.get("points.give.userunknown", targetUsername));
+            return;
+        }
+
+        targetUser.points += points;
+        user.points -= points;
+
+        await this.userService.updateUser(targetUser, user);
+        this.twitchService.sendMessage(channel, Lang.get("points.give.success", user.username, targetUsername, points));
+
+    }
+
+    public getAliases(): ICommandAlias[] {
+        return [
+            { alias: "give", commandName: "givepoints" }
+        ];
+    }
+}
