5ede479202109a60bf9025c3249618c3aaa57452
floele <floele@users.noreply.github.com>
2021-03-26 18:24:06 +0100

    Added !song command for the currently played song

---
+++ server/src/commands/commandScripts/songCommand.ts
@@ -0,0 +1,29 @@
+import { Command } from "../command";
+import { SongService } from "../../services";
+import { ICommandAlias, IUser } from "../../models";
+import { BotContainer } from "../../inversify.config";
+
+export class SongCommand extends Command {
+    private songService: SongService;
+
+    constructor() {
+        super();
+
+        this.songService = BotContainer.get(SongService);
+    }
+
+    public executeInternal(channel: string, user: IUser): void {
+        const songQueue = this.songService.getSongQueue();
+        if (songQueue.length > 0) {
+            this.twitchService.sendMessage(channel, `${songQueue[0].details.title} requested by ${songQueue[0].requestedBy}`);
+        } else {
+            this.twitchService.sendMessage(channel, "There is no song in the queue currently.");
+        }
+    }
+
+    public getAliases(): ICommandAlias[] {
+        return [{ alias: "currentsong", commandName: "song" }];
+    }
+}
+
+export default SongCommand;
