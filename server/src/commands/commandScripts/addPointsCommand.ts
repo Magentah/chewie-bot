4e4e58e2b4ddcac0f4a34ea0a175c38e6bdb6356
floele <floele@users.noreply.github.com>
2021-04-06 17:21:38 +0200

    Added !addpoints command to allow mods adding points to a user

---
+++ server/src/commands/commandScripts/addPointsCommand.ts
@@ -0,0 +1,45 @@
+import { Command } from "../command";
+import { UserService } from "../../services";
+import { ICommandAlias, IUser, UserLevels } from "../../models";
+import { BotContainer } from "../../inversify.config";
+import { Lang } from "../../lang";
+import { PointLogType } from "../../models/pointLog";
+
+export default class AddPointsCommand extends Command {
+    private userService: UserService;
+
+    constructor() {
+        super();
+        this.userService = BotContainer.get(UserService);
+        this.minimumUserLevel = UserLevels.Moderator;
+    }
+
+    public async executeInternal(channel: string, user: IUser, targetUsername: string, points: number) {
+        if (!targetUsername || !points || !Number.isInteger(points)) {
+            this.twitchService.sendMessage(channel, Lang.get("points.add.wrongarguments", user.username));
+            return;
+        }
+
+        let targetUser = await this.userService.getUser(targetUsername);
+        if (!targetUser) {
+            if (await this.twitchService.userExistsInChat(channel, targetUsername)) {
+                targetUser = await this.userService.addUser(targetUsername);
+            }
+        }
+
+        if (!targetUser) {
+            this.twitchService.sendMessage(channel, Lang.get("points.give.userunknown", targetUsername));
+            return;
+        }
+
+        await this.userService.changeUserPoints(targetUser, points, PointLogType.Add);
+        this.twitchService.sendMessage(channel, Lang.get("points.add.success", user.username, targetUsername, points));
+    }
+
+    public getAliases(): ICommandAlias[] {
+        return [
+            { alias: "add", commandName: "addpoints" },
+            { alias: "addchews", commandName: "addpoints" },
+        ];
+    }
+}
