0eb4fa23441ab7e244d6d0a15c2191c69302ce59
ghokane <ghokane@gmail.com>
2021-04-29 13:44:23 -0300

    Cleaning up donationlist.tsx
    Removing unnecessary imports.

--- client/src/components/donationlist/donationlist.tsx
+++ client/src/components/donationlist/donationlist.tsx
@@ -1,28 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import MaterialTable from "material-table"
-import useUser, { UserLevels } from "../../hooks/user";
-import { Grid, TextField, Button, CircularProgress, Box, Card, Accordion, AccordionSummary, Typography, AccordionDetails, Icon } from "@material-ui/core";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import AddIcon from "@material-ui/icons/Add";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { AddToListState } from "../common/addToListState";
-
-const useStyles = makeStyles((theme) => ({
-    addButton: {
-        margin: theme.spacing(2, 0, 2),
-    },
-}));
 
 const DonationList: React.FC<any> = (props: any) => {
     type RowData = {username: string, date: Date, amount: number, message: string};
 
-    const classes = useStyles();
     const [donationlist, setDonationlist] = useState([] as RowData[]);
-    const [user, loadUser] = useUser();
-
-    useEffect(loadUser, []);
 
     useEffect(() => {
         axios.get("/api/donationlist").then((response) => {
@@ -31,7 +14,6 @@ const DonationList: React.FC<any> = (props: any) => {
     }, []);
 
     return <div>
-            {/* {songrequestRules} */}
             <MaterialTable
                 columns = {[
                     { title: "Username", field: "username" },
