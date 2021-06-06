c4d119adabf25648044c8cdc1a9c5da5aa5fa5a9
floele <floele@users.noreply.github.com>
2021-06-06 10:38:36 +0200

    Added !editcmd and allow use count in command list

--- server/src/commands/commandScripts/index.ts
+++ server/src/commands/commandScripts/index.ts
@@ -1,5 +1,6 @@
 export { default as TestCommand } from "./testCommand";
 export { default as AddCmdCommand } from "./addCmdCommand";
+export { default as EditCmdCommand } from "./editCmdCommand";
 export { default as TextCommand } from "./textCommand";
 export { default as DelCmdCommand } from "./delCmdCommand";
 export { default as AddSongCommand } from "./addSongCommand";
