11a873e45bd86a9a207d036e3689139fd145a268
floele <floele@users.noreply.github.com>
2022-04-07 18:14:55 +0200

    Added "duel" and "arena" for !mystats (win/loss count)
    Added "arena" to arguments

--- server/src/commands/commandScripts/myStatsCommand.ts
+++ server/src/commands/commandScripts/myStatsCommand.ts
@@ -1,19 +1,22 @@
 import { Command } from "../command";
 import { EventLogType, IUser } from "../../models";
 import { BotContainer } from "../../inversify.config";
-import { CardsRepository, EventLogsRepository, SonglistRepository } from "../../database";
+import { CardsRepository, EventLogsRepository, SonglistRepository, PointLogsRepository } from "../../database";
+import { PointLogReason, PointLogType } from "../../models/pointLog";
 
 export default class MyStatsCommand extends Command {
     private eventLogsRepository: EventLogsRepository;
     private songlistRepository: SonglistRepository;
     private cardsRepository: CardsRepository;
-    private readonly Arguments = "SongRequest|Sudoku|Redeem|SongPlayed|Songlist|Cards";
+    private readonly Arguments = "SongRequest, Sudoku, Redeem, SongPlayed, Songlist, Cards, Duel, Arena";
+    private readonly pointsLog: PointLogsRepository;
 
     constructor() {
         super();
         this.eventLogsRepository = BotContainer.get(EventLogsRepository);
         this.songlistRepository = BotContainer.get(SonglistRepository);
         this.cardsRepository = BotContainer.get(CardsRepository);
+        this.pointsLog = BotContainer.get(PointLogsRepository);
     }
 
     public async executeInternal(channel: string, user: IUser, eventTypeArgument: string): Promise<void> {
@@ -24,7 +27,22 @@ export default class MyStatsCommand extends Command {
 
         const eventType = eventTypeArgument.toLowerCase();
 
-        if (Object.values(EventLogType).includes(eventType as EventLogType)) {
+        if (eventType === "duel" && user.id) {
+            const duelsWon = await this.pointsLog.getCount(user, PointLogType.Duel, PointLogReason.Win);
+            const duelsDraw = await this.pointsLog.getCount(user, PointLogType.Duel, PointLogReason.Draw);
+            const refundsTotal = await this.pointsLog.getCount(user, PointLogType.Duel, PointLogReason.Refund);
+            const startedTotal = await this.pointsLog.getCount(user, PointLogType.Duel, PointLogReason.None);
+            const completedDuels = startedTotal - refundsTotal;
+            this.twitchService.sendMessage(channel, `${user.username}, won ${duelsWon} out of ${completedDuels} duels (draws: ${duelsDraw})`);
+        } else if (eventType === "arena" && user.id) {
+            const arena1 = await this.pointsLog.getCount(user, PointLogType.Arena, PointLogReason.FirstPlace);
+            const arena2 = await this.pointsLog.getCount(user, PointLogType.Arena, PointLogReason.SecondPlace);
+            const arena3 = await this.pointsLog.getCount(user, PointLogType.Arena, PointLogReason.ThirdPlace);
+            const arenaRefundsTotal = await this.pointsLog.getCount(user, PointLogType.Arena, PointLogReason.Refund);
+            const arenaStartedTotal = await this.pointsLog.getCount(user, PointLogType.Arena, PointLogReason.None);
+            const completedArenas = arenaStartedTotal - arenaRefundsTotal;
+            this.twitchService.sendMessage(channel, `${user.username}, participated in ${completedArenas} arenas. Score: 1st [${arena1}], 2nd [${arena2}], 3rd [${arena3}]`);
+        } else if (Object.values(EventLogType).includes(eventType as EventLogType)) {
             const count = await this.eventLogsRepository.getCount(eventType as EventLogType, user);
             this.twitchService.sendMessage(channel, `${user.username}, your stats for ${eventTypeArgument}: ${count}`);
         } else if (eventType === "songlist" && user.id) {
