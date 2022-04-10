39636b8fcd2b7e9a550172a9dc2f7ede2df1a046
floele <floele@users.noreply.github.com>
2022-04-10 09:56:07 +0200

    Added !songstats and !topFavorites
    Additional commands to get more information about a particular song and top favorite songs overall.

---
+++ server/src/commands/commandScripts/songlist/topFavoritesCommand.ts
@@ -0,0 +1,35 @@
+import { Command } from "../../command";
+import { IUser } from "../../../models";
+import { BotContainer } from "../../../inversify.config";
+import { SonglistRepository } from "../../../database";
+
+export default class TopFavoritesCommand extends Command {
+    private songlistRepository: SonglistRepository;
+
+    constructor() {
+        super();
+        this.songlistRepository = BotContainer.get(SonglistRepository);
+    }
+
+    public async executeInternal(channel: string, user: IUser, numberOfUsers: number): Promise<void> {
+        const userCount = numberOfUsers && Number.isInteger(numberOfUsers) ? Math.min(25, numberOfUsers) : 10;
+
+        const favorites = await this.songlistRepository.getTopFavorites(userCount);
+        if (favorites.length === 0) {
+            this.twitchService.sendMessage(channel, "No favorites defined.");
+            return;
+        }
+
+        let result = "Top favorite songs: ";
+        let counter = 1;
+        for (const topSong of favorites) {
+            result += `#${counter++} ${topSong.title} - ${topSong.artist} [${topSong.numFavorites}] / `
+        }
+
+        this.twitchService.sendMessage(channel, result.substring(0, result.length - 2));
+    }
+
+    public getDescription(): string {
+        return `Displays the top 10 or <number> favorited songs on the songlist. Usage: !topFavorites [<number>]`;
+    }
+}
