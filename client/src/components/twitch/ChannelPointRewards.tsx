baecd1608d266bdccbd1c11ffa4f314c9b7e30bf
floele <floele@users.noreply.github.com>
2022-07-17 10:07:12 +0200

    Added "controlled" column to reward list
    It should be visible whether or not a channel point reward has been created through the bot and can be fully controlled.

--- client/src/components/twitch/ChannelPointRewards.tsx
+++ client/src/components/twitch/ChannelPointRewards.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MaterialTable from "@material-table/core";
 
-type RowData = { twitchRewardId: number, title: string, cost: number, isEnabled: boolean, isGlobalCooldownEnabled: boolean, globalCooldown: number | null, shouldSkipRequestQueue: boolean, associatedRedemption: string };
+type RowData = { twitchRewardId: number, title: string, cost: number, isEnabled: boolean, 
+    isGlobalCooldownEnabled: boolean, globalCooldown: number | null, shouldSkipRequestQueue: boolean,
+    associatedRedemption: string, hasOwnership: boolean };
 
 const ChannelPointRewards: React.FC<any> = (props: any) => {
     const [channelPointRewards, setChannelPointRewards] = useState([] as RowData[]);
@@ -16,7 +18,7 @@ const ChannelPointRewards: React.FC<any> = (props: any) => {
             axios.get("/api/twitch/channelrewards/associations").then((savedChannelPointRewards) => {
                 results = results.map((res: any) => {
                     const foundReward = savedChannelPointRewards.data.find((channelPointReward: any) => channelPointReward.twitchRewardId === res.id);
-                    return {...res, associatedRedemption: foundReward?.associatedRedemption ?? "None"}
+                    return {...res, associatedRedemption: foundReward?.associatedRedemption ?? "None", hasOwnership: foundReward?.hasOwnership }
                 });
 
                 setChannelPointRewards(results);
@@ -37,12 +39,13 @@ const ChannelPointRewards: React.FC<any> = (props: any) => {
                 columns = {[
                     { title: "Channel Point Reward Title", field: "title", editable: "onAdd" },
                     { title: "Cost", field: "cost", editable: "onAdd" },
-                    { title: "Redemption", field: "associatedRedemption", lookup: Object.fromEntries(redemptions.map(x => [x, x])), editable: "always", initialEditValue: "None" }
+                    { title: "Redemption", field: "associatedRedemption", lookup: Object.fromEntries(redemptions.map(x => [x, x])), editable: "always", initialEditValue: "None" },
+                    { title: "Controlled", field: "hasOwnership", editable: "never", type: "boolean" },
                 ]}
                 options = {{
                     paging: false,
                     showTitle: true,
-                    actionsColumnIndex: 3
+                    actionsColumnIndex: 4
                 }}
                 data = {channelPointRewards}
                 editable = {{
