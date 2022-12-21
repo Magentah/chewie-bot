49724e3b5246749f4b4b7513d9c1fa9d44097ab5
floele <floele@users.noreply.github.com>
2022-12-21 14:17:22 +0100

    Fixed order of donations now longer being latest first
    Also fixed error when attempting to disable grouping.

--- client/src/components/donationlist/donationlist.tsx
+++ client/src/components/donationlist/donationlist.tsx
@@ -20,8 +20,8 @@ const DonationList: React.FC<any> = (props: any) => {
                 columns = {[
                     { title: "Username", field: "username" },
                     {
-                         title: "Date", field: "date", defaultGroupOrder: 0, defaultGroupSort: "desc", type: "date",
-                         render: (rowData: any) => <span>{dateFormat.format(new Date(rowData))}</span>
+                         title: "Date", field: "date", defaultGroupOrder: 0, defaultGroupSort: "asc", type: "date",
+                         render: (rowData: any, type: 'row' | 'group') => <span>{dateFormat.format(new Date(type === "group" ? rowData : rowData.date))}</span>
                     },
                     { title: "Message", field: "message" },
                     { title: "Amount", field: "amount", type: "currency" }
