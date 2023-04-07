7691573b1d9572a3cd1e23b03cfafaf3570590d9
floele <floele@gmail.com>
2023-04-07 18:21:39 +0200

    Made AI generated commands editable

--- client/src/components/commands/commandlist.tsx
+++ client/src/components/commands/commandlist.tsx
@@ -13,14 +13,15 @@ import { AddToListState } from "../common/addToListState";
 enum CommandType {
     Text,
     Alias,
-    System
+    System,
+    TextGeneration
 }
 
 type RowData = { id?: number, commandName: string, content: string, type: CommandType, minUserLevel: number, useCount: number, useCooldown: boolean };
 
 const CommandNameCell: React.FC<any> = (value: RowData) => {
     let icon = <Settings />;
-    if (value.type === CommandType.Text) {
+    if (value.type === CommandType.Text || value.type === CommandType.TextGeneration) {
         icon = <Chat />;
     } else if (value.type === CommandType.Alias) {
         icon = <Launch />;
@@ -143,6 +144,16 @@ const CommandList: React.FC<any> = (props: any) => {
                             {userLevels.map(e => <MenuItem key={`edit-level-item-${e.rank}`} value={e.rank}>{e.name}</MenuItem>)}
                         </Select>
                     </FormControl>
+                    <FormControl fullWidth style={{ marginTop: 15 }}>
+                        <InputLabel>Processing</InputLabel>
+                        <Select
+                            value={editingCommand?.type ?? CommandType.Text}
+                            disabled={editingCommand?.type === CommandType.Alias}
+                            onChange={(e: SelectChangeEvent<number>, child: ReactNode) => setEditingCommand({...editingCommand, type: e.target.value as number})}>
+                            <MenuItem key={`edit-type-item-${1}`} value={0}>Plaintext</MenuItem>
+                            <MenuItem key={`edit-type-item-${3}`} value={3}>AI prompt</MenuItem>
+                        </Select>
+                    </FormControl>
                 </DialogContent>
                 <DialogActions>
                     <Button disabled={!editingCommand?.commandName} onClick={() => handleEditCommandClose(true)}>Save</Button>
@@ -169,7 +180,7 @@ const CommandList: React.FC<any> = (props: any) => {
                         editable: (columnDef: any, rowData: RowData) => rowData?.type === CommandType.Text,
                         width: "10%"
                     },
-                    { title: "Type", field: "type", editable: "onAdd", lookup: { 0: "Text", 1: "Alias", 2: "System" }, defaultFilter: ["0", "1"], initialEditValue: 0 },
+                    { title: "Type", field: "type", editable: "onAdd", lookup: { 0: "Text", 1: "Alias", 2: "System", 3: "Generated text" }, defaultFilter: ["0", "1", "3"], initialEditValue: 0 },
                     { title: "Required permissions", field: "minUserLevel", editable: (columnDef: any, rowData: RowData) => rowData?.type === CommandType.Text, lookup: Object.fromEntries(userLevels.map(e => [e.rank, e.name])) }
                 ]}
                 options = {{
