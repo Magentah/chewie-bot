81a38b09f99307f10ae5b8dfa2909b0809cd2f07
floele <floele@gmail.com>
2024-03-19 21:44:05 +0100

    Attempt to fix error TS2344 #2

--- server/src/types/knex/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Knex as KnexOriginal } from 'knex';
-
-declare module 'knex' {
-  namespace Knex {
-    interface QueryBuilder {
-        fulltextSearch<TRecord, TResult>(value: string, columns: string[]): KnexOriginal.QueryBuilder;
-    }
-  }
-}
